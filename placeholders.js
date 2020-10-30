3f2d3e404cf37b4e1b9088f02a7018921c551e1e
Mikhail Voloshn <mikhailvol@webfolks.io>
2020-10-30 12:07:22 +0200

    Create placeholders.js

---
+++ placeholders.js
@@ -0,0 +1,16 @@
+var labels = document.querySelectorAll("p.pd-text label, p.pd-select label, p.pd-textarea label");
+var i = labels.length;
+while (i--) {
+	var label = labels.item(i);
+	var text = label.textContent;
+	label.parentNode.classList.contains("required") && (text += " *");
+	var nextElement = label.nextElementSibling;
+	if (nextElement) {
+		if (nextElement.tagName == 'SELECT') {
+			nextElement.options[0].text = text;
+		} else {
+			nextElement.setAttribute("placeholder", text);
+		}
+		label.parentNode.removeChild(label);
+	}
+}

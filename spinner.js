4dece8a530bd6bed84bc053d4c58ad824b00d10c
Mikhail Voloshn <mikhailvol@webfolks.io>
2020-11-09 12:54:41 +0200

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -1,5 +1,6 @@
 const submit = document.getElementsByClassName("submit")[0];
 submit.onclick = function() {
-  submit.style.color = 'hsla(0, 0%, 100%, 0)';
-  submit.classList.add("submit--spinner");
+  const submitBtn = document.getElementsByClassName("submit")[0];
+  submitBtn.style.color = 'hsla(0, 0%, 100%, 0)';
+  submitBtn.classList.add("submit--spinner");
 };
